57f7a1e382ed74451b142787174cbc193edad4e7
GOKHAN ASKIN <90231677+gaskin23@users.noreply.github.com>
2022-07-01 12:22:52 +0300

    UPDATED İNDEX

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello MINDBEHIND" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "I HAVE COMPLETED MY CASE" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
